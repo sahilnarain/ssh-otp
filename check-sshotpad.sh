c5b3a6f1db3f1d635e5d206db40420aab2dc4a5c
Sahil Narain <sahil.narain@gmail.com>
2023-10-17 19:23:15 +0000

    Fixed typo in generate.sh path

--- check-sshotpad.sh
+++ check-sshotpad.sh
@@ -4,6 +4,6 @@ login_sha256=`journalctl -u sshd | grep 'Accepted publickey' | tail -n 1 | awk -
 grep 'sshotpad' ~/.ssh/authorized_keys | while IFS= read -r line; do
   pubkey_sha256=`ssh-keygen -lf /dev/stdin -E sha256 <<< $line | awk -F':' '{print $2}' | awk '{print $1}'`
     if [ $login_sha256 == $pubkey_sha256 ]; then
-      ~/generate.sh
+      ./generate.sh
     fi
 done
