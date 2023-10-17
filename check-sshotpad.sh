87e881c385d6a90e2e70e46a35dbe4f003c2e3f5
Sahil Narain <sahil.narain@gmail.com>
2023-10-17 19:02:28 +0000

    Execute only if shhotpad key is used

---
+++ check-sshotpad.sh
@@ -0,0 +1,9 @@
+login_sha256=`journalctl -u sshd | grep 'Accepted publickey' | tail -n 1 | awk -F':' '{print $NF}'`
+
+
+grep 'sshotpad' ~/.ssh/authorized_keys | while IFS= read -r line; do
+  pubkey_sha256=`ssh-keygen -lf /dev/stdin -E sha256 <<< $line | awk -F':' '{print $2}' | awk '{print $1}'`
+    if [ $login_sha256 == $pubkey_sha256 ]; then
+      ~/generate.sh
+    fi
+done
